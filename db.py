94693e2cc2642170ee2f71de57a7ba043b714628
Amir Ahmetov <amirahmetov@Amirs-MacBook-Pro.local>
2025-06-22 02:46:59 +0500

    upgrade SQL requers for search by IIN

--- db.py
+++ db.py
@@ -14,27 +14,33 @@ async def get_pool():
 async def get_person_by_iin(pool, iin: str):
     query = """
     SELECT 
-        p.id,
-        p.surname,
-        p.name,
-        p.patronymic,
-        p.gender,
-        p.birth_date,
-        p.identifier,
-        p.iin,
-        p.citizenship,
-        p.nationality,
-        p.address,
-        p.address_confirmed,
-        p.residence_start,
-        p.residence_end,
-        STRING_AGG(pn.raw_number, ', ') AS all_raw_numbers,
-        STRING_AGG(pn.normalized_number, ', ') AS all_normalized_numbers
-    FROM people p
-    LEFT JOIN phone_numbers pn ON pn.person_id = p.id
-    WHERE p.iin = $1
-    GROUP BY p.id
-    LIMIT 1
+    p.id,
+    p.surname,
+    p.name,
+    p.patronymic,
+    p.gender,
+    p.birth_date,
+    p.identifier,
+    p.iin,
+    p.citizenship,
+    p.nationality,
+    p.address,
+    p.address_confirmed,
+    p.residence_start,
+    p.residence_end,
+    (
+        SELECT STRING_AGG(pn.raw_number, ', ') 
+        FROM phone_numbers pn 
+        WHERE pn.person_id = p.id
+    ) AS all_raw_numbers,
+    (
+        SELECT STRING_AGG(pn.normalized_number, ', ') 
+        FROM phone_numbers pn 
+        WHERE pn.person_id = p.id
+    ) AS all_normalized_numbers
+FROM people p
+WHERE p.iin = $1
+LIMIT 1;
     """
     async with pool.acquire() as conn:
         return await conn.fetchrow(query, iin)
